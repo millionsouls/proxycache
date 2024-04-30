65363f206c0916b14ca2f45417aa87b206bef19e
Kevin W <36448536+kevinw7789@users.noreply.github.com>
2024-04-30 18:43:01 -0400

    content

--- HttpRequest.java
+++ HttpRequest.java
@@ -9,6 +9,7 @@
  import java.net.*;
  import java.util.*;
  
+ @SuppressWarnings("unused")
  public class HttpRequest {
      /** Help variables */
      final static String CRLF = "\r\n";
@@ -23,72 +24,73 @@
      private int port;
  
      /** Create HttpRequest by reading it from the client socket */
-     public HttpRequest(BufferedReader from) {
-     String firstLine = "";
-     try {
-         firstLine = from.readLine();
-     } catch (IOException e) {
-         System.out.println("Error reading request line: " + e);
-     }
- 
-     String[] tmp = firstLine.split(" ");
-     method = /* Fill in */;
-     URI = /* Fill in */;
-     version = /* Fill in */;
- 
-     System.out.println("URI is: " + URI);
- 
-     if (!method.equals("GET")) {
-         System.out.println("Error: Method not GET");
-     }
-     try {
-         String line = from.readLine();
-         while (line.length() != 0) {
-         headers += line + CRLF;
-         /* We need to find host header to know which server to
-          * contact in case the request URI is not complete. */
-         if (line.startsWith("Host:")) {
-             tmp = line.split(" ");
-             if (tmp[1].indexOf(':') > 0) {
-             String[] tmp2 = tmp[1].split(":");
-             host = tmp2[0];
-             port = Integer.parseInt(tmp2[1]);
-             } else {
-             host = tmp[1];
-             port = HTTP_PORT;
-             }
-         }
-         line = from.readLine();
-         }
-     } catch (IOException e) {
-         System.out.println("Error reading from socket: " + e);
-         return;
-     }
-     System.out.println("Host to contact is: " + host + " at port " + port);
-     }
+    public HttpRequest(BufferedReader from) {
+        String firstLine = "";
+        try {
+            firstLine = from.readLine();
+        } catch (IOException e) {
+            System.out.println("Error reading request line: " + e);
+        }
+    
+        String[] tmp = firstLine.split(" ");
+        method = tmp[0];
+        URI = tmp[1];
+        version = tmp[2];
+    
+		System.out.println("Reading request...");
+        System.out.println("URI is: " + URI);
+    
+        if (!method.equals("GET")) {
+            System.out.println("Error: Method not GET");
+        }
+        try {
+            String line = from.readLine();
+            while (line.length() != 0) {
+            headers += line + CRLF;
+            /* We need to find host header to know which server to
+            * contact in case the request URI is not complete. */
+            if (line.startsWith("Host:")) {
+                tmp = line.split(" ");
+                if (tmp[1].indexOf(':') > 0) {
+                    String[] tmp2 = tmp[1].split(":");
+                    host = tmp2[0];
+                    port = Integer.parseInt(tmp2[1]);
+                } else {
+                    host = tmp[1];
+                    port = HTTP_PORT;
+                }
+            }
+            line = from.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading from socket: " + e);
+            return;
+        }
+        System.out.println("Host to contact is: " + host + " at port " + port);
+    }
  
      /** Return host for which this request is intended */
-     public String getHost() {
-     return host;
-     }
+    public String getHost() {
+        return host;
+    }
  
      /** Return port for server */
-     public int getPort() {
-     return port;
-     }
+    public int getPort() {
+        return port;
+    }
  
      /**
       * Convert request into a string for easy re-sending.
       */
-     public String toString() {
-     String req = "";
- 
-     req = method + " " + URI + " " + version + CRLF;
-     req += headers;
-     /* This proxy does not support persistent connections */
-     req += "Connection: close" + CRLF;
-     req += CRLF;
-     
-     return req;
-     }
+    public String toString() {
+        String req = "";
+    
+        req = method + " " + URI + " " + version + CRLF;
+        req += headers;
+        /* This proxy does not support persistent connections */
+        req += "Connection: close" + CRLF;
+        req += CRLF;
+        
+        return req;
+    }
  }
--- HttpResponse.java
+++ HttpResponse.java
@@ -9,6 +9,7 @@
  import java.net.*;
  import java.util.*;
  
+ @SuppressWarnings("unused")
  public class HttpResponse {
      final static String CRLF = "\r\n";
      /** How big is the buffer used for reading the object */
@@ -26,91 +27,88 @@
  
      /** Read response from server. */
      public HttpResponse(DataInputStream fromServer) {
-     /* Length of the object */
-     int length = -1;
-     boolean gotStatusLine = false;
- 
-     /* First read status line and response headers */
-     try {
-         String line = /* Fill in */;
-         while (line.length() != 0) {
-         if (!gotStatusLine) {
-             statusLine = line;
-             gotStatusLine = true;
-         } else {
-             headers += line + CRLF;
-         }
- 
-         /* Get length of content as indicated by
-          * Content-Length header. Unfortunately this is not
-          * present in every response. Some servers return the
-          * header "Content-Length", others return
-          * "Content-length". You need to check for both
-          * here. */
-         if (line.startsWith(/* Fill in */) ||
-             line.startsWith(/* Fill in */)) {
-             String[] tmp = line.split(" ");
-             length = Integer.parseInt(tmp[1]);
-         }
-         line = fromServer.readLine();
-         }
-     } catch (IOException e) {
-         System.out.println("Error reading headers from server: " + e);
-         return;
+        /* Length of the object */
+        int length = -1;
+        boolean gotStatusLine = false;
+    
+        /* First read status line and response headers */
+        try {
+            String line = fromServer.readLine();
+            while (line.length() != 0) {
+            if (!gotStatusLine) {
+                statusLine = line;
+                gotStatusLine = true;
+            } else {
+                headers += line + CRLF;
+            }
+    
+            /* Get length of content as indicated by
+            * Content-Length header. Unfortunately this is not
+            * present in every response. Some servers return the
+            * header "Content-Length", others return
+            * "Content-length". You need to check for both
+            * here. */
+            if (line.toLowerCase().startsWith("content-length")) {
+                String[] tmp = line.split(" ");
+                length = Integer.parseInt(tmp[1]);
+            }
+            line = fromServer.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading headers from server: " + e);
+            return;
+        }
+    
+        try {
+            int bytesRead = 0;
+            byte buf[] = new byte[BUF_SIZE];
+            boolean loop = false;
+    
+            /* If we didn't get Content-Length header, just loop until
+            * the connection is closed. */
+            if (length == -1) {
+            loop = true;
+            }
+            
+            /* Read the body in chunks of BUF_SIZE and copy the chunk
+            * into body. Usually replies come back in smaller chunks
+            * than BUF_SIZE. The while-loop ends when either we have
+            * read Content-Length bytes or when the connection is
+            * closed (when there is no Connection-Length in the
+            * response. */
+            while (bytesRead < length || loop) {
+                /* Read it in as binary data */
+                int res = fromServer.read(buf);
+                if (res == -1) {
+                    break;
+                }
+                /* Copy the bytes into body. Make sure we don't exceed
+                * the maximum object size. */
+                for (int i = 0; i < res && (i + bytesRead) < MAX_OBJECT_SIZE; i++) {
+                    body[i + bytesRead] = buf[i];
+                }
+                bytesRead += res;
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading response body: " + e);
+            return;
+        }
+    
+    
      }
- 
-     try {
-         int bytesRead = 0;
-         byte buf[] = new byte[BUF_SIZE];
-         boolean loop = false;
- 
-         /* If we didn't get Content-Length header, just loop until
-          * the connection is closed. */
-         if (length == -1) {
-         loop = true;
-         }
-         
-         /* Read the body in chunks of BUF_SIZE and copy the chunk
-          * into body. Usually replies come back in smaller chunks
-          * than BUF_SIZE. The while-loop ends when either we have
-          * read Content-Length bytes or when the connection is
-          * closed (when there is no Connection-Length in the
-          * response. */
-         while (bytesRead < length || loop) {
-         /* Read it in as binary data */
-         int res = /* Fill in */;
-         if (res == -1) {
-             break;
-         }
-         /* Copy the bytes into body. Make sure we don't exceed
-          * the maximum object size. */
-         for (int i = 0; 
-              i < res && (i + bytesRead) < MAX_OBJECT_SIZE; 
-              i++) {
-             /* Fill in */
-         }
-         bytesRead += res;
-         }
-      } catch (IOException e) {
-          System.out.println("Error reading response body: " + e);
-          return;
-      }
- 
- 
-     }
- 
-     /**
-      * Convert response into a string for easy re-sending. Only
-      * converts the response headers, body is not converted to a
-      * string.
-      */
-     public String toString() {
-     String res = "";
- 
-     res = statusLine + CRLF;
-     res += headers;
-     res += CRLF;
-     
-     return res;
-     }
+    
+        /**
+         * Convert response into a string for easy re-sending. Only
+        * converts the response headers, body is not converted to a
+        * string.
+        */
+    public String toString() {
+        String res = "";
+    
+        res = statusLine + CRLF;
+        res += headers;
+        res += CRLF;
+        
+        return res;
+    }
  }
